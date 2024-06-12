89f3f7aab4d891c3a6c87a914a6e315223f9ccd8
정삼복 <68502584+3boku@users.noreply.github.com>
2024-06-12 20:33:27 +0900

    Image 업로더 완성

--- services/postImage.go
+++ services/postImage.go
@@ -1,7 +1,31 @@
 package services
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/olahol/go-imageupload"
+	"net/http"
+)
+
+var currentImage *imageupload.Image
 
 func PostImage(c *gin.Context) {
+	file, err := c.FormFile("image")
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
 
+	err = c.SaveUploadedFile(file, "/imageData"+file.Filename)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success",
+	})
 }
