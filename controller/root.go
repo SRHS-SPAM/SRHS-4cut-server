284d4e85b2f58160a53e0ad60184ca56c4627197
정삼복 <68502584+3boku@users.noreply.github.com>
2024-06-12 19:00:08 +0900

    root 컨트롤러 생성

--- controller/root.go
+++ controller/root.go
@@ -1,5 +1,16 @@
 package controller
 
-func RootController() {
+import (
+	"SRHS-4cut-server/services"
+	"github.com/gin-gonic/gin"
+)
 
+func RootController(r *gin.Engine) {
+	r.POST("/", func(c *gin.Context) {
+		services.GetImage(c)
+	})
+
+	r.GET("/", func(c *gin.Context) {
+
+	})
 }
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
-package SRHS_4cut_server
+package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"SRHS-4cut-server/controller"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	
+
+	controller.RootController(r)
+
 	err := r.Run(":8080")
 	if err != nil {
 		panic(err)
